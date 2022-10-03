65a20242b47616851e3c1d38ac8c675bd798d5bb
doublejone831 <69640013+doublejone831@users.noreply.github.com>
2022-10-03 16:48:27 +0900

    [BOJ]11048/이동하기/실2/3분

--- 장지원/3주차/[BOJ]11048_이동하기.cpp
+++ 장지원/3주차/[BOJ]11048_이동하기.cpp
@@ -1,1 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
 
+int main(){
+    int N,M;
+    cin >> N >> M;
+    vector<vector<int>> arr(N,vector<int>(M));
+    vector<vector<int>> dp(N,vector<int>(M,0));
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < M; j++){
+            cin >> arr[i][j];
+        }
+    }
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < M; j++){
+            if(i == 0 && j == 0){
+                dp[i][j] = arr[i][j];
+            }
+            else if (j ==0){
+                dp[i][j] = dp[i-1][j]+arr[i][j];
+            }
+            else if (i == 0){
+                dp[i][j] = dp[i][j-1]+arr[i][j];
+            }
+            else{
+                dp[i][j] = max(max(dp[i-1][j-1],dp[i-1][j]),dp[i][j-1]) + arr[i][j];
+            }
+        }
+    }
+    cout << dp[N-1][M-1];
+}
